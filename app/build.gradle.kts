0caf38719c4e3da5c926cd2c8cffecc7da3a479a
Jwaxy <98110672+jwaxy@users.noreply.github.com>
2023-04-24 15:28:21 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "it.vfsfitvnm.vimusic"
-        minSdk = 21
+        minSdk = 29
         targetSdk = 32
         versionCode = 25
         versionName = "0.4.13"
